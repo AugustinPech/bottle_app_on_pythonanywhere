44ed20066a4de60792d8296393acea52024ad1cb
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-05-30 14:20:44 +0200

    Déploiement automatique - 2024-05-30 14:20:44

--- bottle_app.py
+++ bottle_app.py
@@ -1,9 +1,13 @@
 
 # A very simple Bottle Hello World app for you to get started with...
+
 from bottle import default_app, route, run, template, error, static_file
+
+
+abs_views_path= os.path.join(os.getcwd(), 'views')
 
 @route('/')
 def hello_world():
-    return 'Welcome to the JL\'s buddys!'
+    return static_file('index.html', root=abs_views_path)
 
 application = default_app()
